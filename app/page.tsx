d9dc5dbe52ea3fa42caca7482e1c0da568c057cc
dianarojina <dianarojina88@gmail.com>
2024-05-06 09:34:35 +0900

    check

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <iframe src="https://www.google.com/maps/embed?pb=!4v1701050215012!6m8!1m7!1s9PmIgp9NPwDUEvGl9RTzBg!2m2!1d62.02762971978814!2d129.7316531083102!3f144.22146413197663!4f-2.6612194751688634!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  return 'Hello world'
 }
