f78b150aab913366316634c3b13f98641a229bcc
dianarojina <dianarojina88@gmail.com>
2024-05-06 12:13:34 +0900

    styles fixed_7

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import styles from '../styles/styles_for_main_page.css'
 export default function Page() {
   return (
-    <div className={styles.main}>
+    <div className={`${styles.main}`}>
       <h1>Where am I?</h1>
 
     </div>
