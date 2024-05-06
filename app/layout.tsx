572a2a0b7627d031b5802173302c3a99012859df
dianarojina <dianarojina88@gmail.com>
2024-05-06 10:49:22 +0900

    check_6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import Page from './page.js'
+import React from 'react';
+
+const RootLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <Page />
+        {children}
+      </body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
---
+++ app/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>tyt bila stranitsa 0</h1>
+  );
+};
