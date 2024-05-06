4de6695261e6a71fb4fd4bfff75dbb9701852000
dianarojina <dianarojina88@gmail.com>
2024-05-06 10:12:02 +0900

    check_4or5

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <style>
+        .div1 {
+          background-color: red;
+          width: 100px;
+          height: 100px;
+        }
+      </style>
+      <body>
+        <div class="div1">
+            red
+        </div>
+      </body>
     </html>
   )
 }
