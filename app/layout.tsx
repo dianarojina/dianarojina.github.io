cd45798c05857ac5d9542eb1e98f9637b9e28476
dianarojina <dianarojina88@gmail.com>
2024-05-06 10:59:44 +0900

    check_7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,12 @@
-import Page from './page.js'
-import React from 'react';
-
-const RootLayout = ({ children }) => {
+import "./styles.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html>
-      <body>
-        <Page />
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  );
-};
-
-export default RootLayout;
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en">
-//       <body>{children}</body>
-//     </html>
-//   )
-// }
+  )
+}
