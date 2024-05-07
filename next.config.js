d39e12975787476c3ab762ff300561591ed663a4
dianarojina <dianarojina88@gmail.com>
2024-05-07 10:26:54 +0900

    next.config edited

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
+// const nextConfig = {
+//   output: 'export'
+// }
+// module.exports = nextConfig
+
 const nextConfig = {
-  output: 'export'
-}
-module.exports = nextConfig
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            modules: true,
+          },
+        },
+      ],
+      include: /\.module\.css$/,
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
