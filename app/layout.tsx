5a5e3568e7b8064d37fe57f5faa819df1e3c5694
dianarojina <dianarojina88@gmail.com>
2024-05-07 09:26:55 +0900

    styles fix_9

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
       <style> {`
         .main {
           background-color: #faffbf;
@@ -15,6 +16,7 @@ export default function RootLayout({
         }
       `}
       </style>
+      </head>
       <body>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ export default function Page() {
   return (
     <div class="main">
       <h1>Where am I?</h1>
-        
+        <div class="wind">
+          
+        </div>
     </div>
   );
 };
