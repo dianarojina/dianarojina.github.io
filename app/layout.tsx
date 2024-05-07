69120456462df9392e6685af3ffa8205b2a341aa
dianarojina <dianarojina88@gmail.com>
2024-05-07 09:23:57 +0900

    styles fix_8

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <style> {`
+        .main {
+          background-color: #faffbf;
+          width: 100%;
+          height: 100%;
+          margin: auto;
+        }
+      `}
+      </style>
       <body>{children}</body>
     </html>
   )
