f4e3984abb9be8c4d3a4c3f9e9356b82bfe35325
dianarojina <dianarojina88@gmail.com>
2024-05-13 10:39:52 +0900

    add google lib

--- app/loadGoogleMaps.js
+++ app/loadGoogleMaps.js
@@ -1,7 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const API_KEY = process.env.API_KEY;
 
 const StreetView = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const loader = useRef(null);
+
   useEffect(() => {
+    loader.current = new Loader({
+      apiKey: API_KEY,
+      version: 'weekly',
+    });
+
+    loader.current.load().then(() => {
+      setIsLoaded(true);
+      initStreetView();
+    });
+  }, []);
+
+  const initStreetView = () => {
     const fenway = { lat: 42.345573, lng: -71.098326 };
     const panorama = new window.google.maps.StreetViewPanorama(
       document.getElementById('pano'),
@@ -13,9 +31,13 @@ const StreetView = () => {
         },
       }
     );
-  }, []);
+  };
 
-  return <div id="pano" style={{ height: '500px', width: '100%' }} />;
+  return isLoaded ? (
+    <div id="pano" style={{ height: '500px', width: '100%' }} />
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 export default StreetView;
