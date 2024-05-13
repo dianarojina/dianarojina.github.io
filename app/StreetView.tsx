7ff194ed25255439c60e7c8a15d2caec6b28a39a
Vladimir Leveryev <volodya.leveryev@gmail.com>
2024-05-14 08:22:59 +0900

    Fix

--- app/StreetView.tsx
+++ app/StreetView.tsx
@@ -4,8 +4,8 @@ import { useEffect } from 'react';
 const StreetView = () => {
   useEffect(() => {
     const fenway = { lat: 62.02757, lng: 129.731495 };
-    const panorama = new window.google.maps.StreetViewPanorama(
-      document.getElementById('pano'),
+    const panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano') as HTMLElement,
       {
         position: fenway,
         pov: {
